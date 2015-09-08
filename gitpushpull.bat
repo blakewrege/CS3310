ed25ecdb3c08f4df25b8842ed96af9ba0e621b10
blake <blakewrege1@gmail.com>
2015-09-08 11:53:13 -0400

    edited thet repo

--- gitpushpull.bat
+++ gitpushpull.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Trys to pull then push to github
-git pull
-git add *
+git pull origin master
+git add -A
 set /p gitcomment= Enter Comment: 
 git commit -m %gitcomment%
-git push 
+git push origin master
 echo Done
 pause
